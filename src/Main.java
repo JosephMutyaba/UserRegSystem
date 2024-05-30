ef74b9fe483064cadbe49f65bc0196efb87549a2
Joseph <josephsteinson@gmail.com>
2024-05-30 19:05:33 +0300

    Added functionality for entry point, overriden the toString, equals and hashCode methods of the userModel class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // print the menu for user to choose
+        System.out.println("USER REG SYSTEM" +
+                "\n1.Register user" +
+                "\n2.List all users" +
+                "\n3.Get a user by username" +
+                "\n4.Delete all users" +
+                "\n5.Delete a user by username" +
+                "\n6.Update user details" +
+                "\n7.Exit");
     }
 }
--- src/model/User.java
+++ src/model/User.java
@@ -1,14 +1,21 @@
 package model;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class User {
     // attributes
     private String firstName;
     private String lastName;
     private String userName;
-    private String dateOfBirth;
+    private Date dateOfBirth;
+
+    // default constructor
+    public User() {
+    }
 
     // constructor
-    public User(String firstName, String lastName, String userName, String dateOfBirth) {
+    public User(String firstName, String lastName, String userName, Date dateOfBirth) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.userName = userName;
@@ -40,11 +47,59 @@ public class User {
         this.userName = userName;
     }
 
-    public String getDateOfBirth() {
+    public Date getDateOfBirth() {
         return dateOfBirth;
     }
 
-    public void setDateOfBirth(String dateOfBirth) {
+    public void setDateOfBirth(Date dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
+    }
+
+    // Override for the toString method
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", userName='" + userName + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int result =17; // starting with 17
+        if (this.userName != null) {
+            result = 31 * result + userName.hashCode();
+        }
+        if (this.firstName != null) {
+            result = 31 * result + firstName.hashCode();
+        }
+        if (this.lastName != null) {
+            result = 31 * result + lastName.hashCode();
+        }if (this.dateOfBirth != null) {
+            result = 31 * result + dateOfBirth.hashCode();
+        }
+        return result ;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        User usr = (User) obj;
+
+        return this.dateOfBirth != null ? this.dateOfBirth.equals(usr.dateOfBirth) : usr.dateOfBirth == null &&
+                this.firstName != null ? this.firstName.equals(usr.firstName): usr.firstName ==null &&
+                this.lastName != null ? this.lastName.equals(usr.lastName): usr.lastName ==null &&
+                this.userName != null ? this.userName.equals(usr.userName): usr.userName ==null;
+
     }
 }
